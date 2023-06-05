dae8cd4a295cdc40c2b4c67dcab0da3836acf317
haeram27 <haeram27@gmail.com>
2023-06-05 14:21:03 +0900

    update etc

--- 02init/_2_nosudo_setup_conf.sh
+++ 02init/_2_nosudo_setup_conf.sh
@@ -4,8 +4,8 @@
 myid=$(whoami)
 
 # terminator user conf
-test -d ~/.config/terminator/ || mkdir ~/.config/terminator/
+test -d ~/.config/terminator || mkdir -p ~/.config/terminator
 cp -b ./conf/terminator/config ~/.config/terminator/config
-chown -R $myid:$myid ~/.config/terminator/
+chown -R $myid:$myid ~/.config/terminator
 chmod 664 ~/.config/terminator/config
 
