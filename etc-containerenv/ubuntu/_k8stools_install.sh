0df0576063630d8adc8938d2d52101298a180b43
haeram27 <haeram27@gmail.com>
2024-03-14 16:08:20 +0900

    update container install script

--- etc-containerenv/ubuntu/_k8stools_install.sh
+++ etc-containerenv/ubuntu/_k8stools_install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ "$EUID" -ne 0 ]]; then
-    echo 'error: please run as root'
-    echo 'ex) sudo docker-install-ubuntu.sh ${USER}'
+    echo "error: please run as root"
+    echo "ex) sudo $0"
     exit 1
 fi
 
+sudo rm -f /etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg
+sudo rm -f /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/$(curl -Ls https://dl.k8s.io/release/stable.txt)/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$(curl -Ls https://dl.k8s.io/release/stable.txt)/deb/ /" |  $ sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$(curl -Ls https://dl.k8s.io/release/stable.txt | grep -Po '^v\d+.\d+')/deb/Release.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg
+echo deb '[signed-by=/etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/'$(curl -Ls https://dl.k8s.io/release/stable.txt | grep -Po "^v\d+.\d+")'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get remove -y kubelet kubeadm kubectl
 sudo apt-get install -y kubelet kubeadm kubectl
