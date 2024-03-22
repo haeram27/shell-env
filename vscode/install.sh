ca76f277f771c02a08386f62bedd0d40f83f3010
haeram27 <haeram27@gmail.com>
2024-03-22 16:34:53 +0900

    fix paths

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+[[ -d /home/${USER}/.vscode/data/Machine ]] && \
+cp ./linux.settings.json   /home/${USER}/.vscode/data/Machine/settings.json 
+[[ -d /home/${USER}/.vscode-server/data/Machine ]] && \
 cp ./linux.settings.json   /home/${USER}/.vscode-server/data/Machine/settings.json 
 cp -r ./style-guide /home/${USER}/.vscode-server/ 
 
