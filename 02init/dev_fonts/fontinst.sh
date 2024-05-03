8dc2724a4e40202cbd2ae93921b83d5cea4cec85
haeram27 <haeram27@gmail.com>
2024-05-03 13:41:14 +0900

    update font settings

--- 02init/dev_fonts/fontinst.sh
+++ 02init/dev_fonts/fontinst.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+tar xvf d2coding.txz
+tar xvf meslo.txz
+tar xvf windows.txz
+
 chmod -R 755 ./
 
 cp -rf d2coding /usr/share/fonts/truetype
@@ -7,3 +11,7 @@ cp -rf meslo /usr/share/fonts/truetype
 cp -rf windows  /usr/share/fonts/truetype
 
 fc-cache -vf
+
+rm -rf ./d2coding
+rm -rf ./meslo
+rm -rf ./windows
--- 02init/dev_fonts/fontinstwsl.sh
+++ 02init/dev_fonts/fontinstwsl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+tar xvf d2coding.txz
+tar xvf meslo.txz
+tar xvf windows.txz
+
 chmod -R 755 ./
 
 cp -rf d2coding /usr/share/fonts/truetype
@@ -8,3 +12,7 @@ cp -f ./etc_fonts_local.conf /etc/fonts/local.conf
 chmod 644 /etc/fonts/local.conf
 
 fc-cache -vf
+
+rm -rf ./d2coding
+rm -rf ./meslo
+rm -rf ./windows
