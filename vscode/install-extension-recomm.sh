c5e3636616bbb9a7c05bd89f834a25fd3a44fcf4
haeram27 <haeram27@gmail.com>
2024-05-13 10:06:58 +0900

    update recom extensions

--- vscode/install-extension-recomm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TARGET="/home/${USER}/.vscode/extensions"
-if [[ -d $TARGET ]]; then
-  cp $TARGET/extensions.json $TARGET/extensions.json.bak
-  cp ./extensions.recommed.linux.json $TARGET/extensions.json
-fi
-
-
