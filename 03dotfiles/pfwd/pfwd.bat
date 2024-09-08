19ee5b12cb1c3276480109abca80ebeb21832999
haeram27 <haeram27@gmail.com>
2024-09-08 19:36:04 +0900

    update alias

--- 03dotfiles/pfwd/pfwd.bat
+++ 03dotfiles/pfwd/pfwd.bat
@@ -1,5 +1,8 @@
 @echo off
 @rem C:\Users\<user-name>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+@rem netstat -n
+@rem tasklist /fi "IMAGENAME eq ssh.exe"
+@rem taskkill /f /t /im ssh.exe
 
 @rem socks5
 start /B ssh -fCN -D localhost:9999 testpub.nat.proxy -o ExitOnForwardFailure=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=10
