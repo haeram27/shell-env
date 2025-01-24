ee9d5b260da780d4f2c9e4d37336049a0e6cabf7
haeram27 <haeram27@gmail.com>
2025-01-24 19:23:21 +0900

    add acinema fonts

--- 02init/dev_fonts/fontinst.sh
+++ 02init/dev_fonts/fontinst.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+tar xvf acinema.txz
 tar xvf d2coding.txz
 tar xvf meslo.txz
 tar xvf windows.txz
 
 chmod -R 755 ./
 
+cp -rf acinema /usr/share/fonts/truetype
 cp -rf d2coding /usr/share/fonts/truetype
 cp -rf meslo /usr/share/fonts/truetype
 cp -rf windows  /usr/share/fonts/truetype
 
 fc-cache -vf
 
+rm -rf ./acinema
 rm -rf ./d2coding
 rm -rf ./meslo
 rm -rf ./windows
--- 02init/dev_fonts/fontinstwsl.sh
+++ 02init/dev_fonts/fontinstwsl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+tar xvf acinema.txz
 tar xvf d2coding.txz
 tar xvf meslo.txz
 tar xvf windows.txz
 
 chmod -R 755 ./
 
+cp -rf acinema /usr/share/fonts/truetype
 cp -rf d2coding /usr/share/fonts/truetype
 cp -rf meslo /usr/share/fonts/truetype
 cp -f ./etc_fonts_local.conf /etc/fonts/local.conf
@@ -13,6 +15,7 @@ chmod 644 /etc/fonts/local.conf
 
 fc-cache -vf
 
+rm -rf ./acinema
 rm -rf ./d2coding
 rm -rf ./meslo
 rm -rf ./windows
