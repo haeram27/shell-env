af0213621bba1e80409e693c60e83031a76c3819
haeram27 <haeram27@gmail.com>
2025-01-26 19:46:17 +0900

    update windows font

--- 02init/dev_fonts/fontinst.bat
+++ 02init/dev_fonts/fontinst.bat
@@ -11,12 +11,15 @@ set REG_KEY="HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
 
 set DIRD2=d2coding
 set DIRMES=meslo
-set ALL=%DIRD2% %DIRMES%
+set DIRACMA=acinema
+set ALL=%DIRD2% %DIRMES% %DIRACMA%
 
 xcopy %DIRD2%\* %DIRSYSFONT% 
+xcopy %DIRMES%\* %DIRSYSFONT% 
+xcopy %DIRACMA%\* %DIRSYSFONT% 
 
 FOR /F "tokens=* usebackq" %%F IN (`dir /b %ALL%`) DO (
-reg add %REG_KEY% /v "%%~nF (TrueType)" /t REG_SZ /d %%F /f
+    reg add %REG_KEY% /v "%%~nF (TrueType)" /t REG_SZ /d %%F /f
 )
 
 pause
