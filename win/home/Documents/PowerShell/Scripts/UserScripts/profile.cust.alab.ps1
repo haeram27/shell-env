6f5799afd752e2ed2f665d4a16917f0c72ee9ef1
haeram27 <haeram27@gmail.com>
2026-09-09 17:21:51 +0900

    2026-09-09 17:21:51+09:00

--- win/home/Documents/PowerShell/Scripts/UserScripts/profile.cust.alab.ps1
+++ win/home/Documents/PowerShell/Scripts/UserScripts/profile.cust.alab.ps1
@@ -1,4 +1,24 @@
-function clean-java-cache {
+function clean-cache-mavenlocal {
+    $mavenPath  = "~\.m2\repository"
+
+    # Maven 저장소 처리
+    if (Test-Path $mavenPath) {
+        Remove-Item -Path $mavenPath -Recurse -Force *> $null
+        Write-Host "✔ Maven 로컬 라이브러리를 삭제했습니다." -ForegroundColor Cyan
+    }
+}
+
+function clean-cache-gradle {
+    $gradlePath = "~\.gradle\caches\modules-2\files-2.1"
+
+    # Gradle 캐시 처리
+    if (Test-Path $gradlePath) {
+        Remove-Item -Path $gradlePath -Recurse -Force *> $null
+        Write-Host "✔ Gradle 캐시 디렉토리를 삭제했습니다." -ForegroundColor Cyan
+    }
+}
+
+function clean-cache-java {
     $gradlePath = "~\.gradle\caches\modules-2\files-2.1"
     $mavenPath  = "~\.m2\repository"
 
